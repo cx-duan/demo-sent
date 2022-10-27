a0dec0b520f1a179998ec2b3939a3dff3d745bd5
cx duan <57568318+cx-duan@users.noreply.github.com>
2022-10-27 16:30:00 +1100

    refactoring

--- sentiment-entity-overview.py
+++ sentiment-entity-overview.py
@@ -5,8 +5,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 
-# Streamlit command to run: python3 -m streamlit run sentiment-entity-overview.py 
-
 #JSON file read. Can be replaced with upload
 with open('response.json', 'r') as transcript_json:
     transcript_data = json.load(transcript_json)
@@ -22,7 +20,7 @@ sent_to_num = {
     "NEGATIVE": -1
 }
 
-# Flatten function to remove redundant zeros
+# Flatten function to remove redundant zeros and help collect average for each entity
 def flatten(S):
     if S == []:
         return S
@@ -35,7 +33,7 @@ def get_sentiment(start, end, entity):
     sent = []
     ent_arr = []
     for sentiment in sentiment_analysis_results:
-        # Add JSON keyvalue to list if sentiment is between the start and end time specified 
+        # Add JSON key-value to list if sentiment is between the start and end time specified 
         if int(sentiment['start']) >= start and int(sentiment['end'] <= end):
             sent.append(sentiment)
         # Extract the sentitment from the key value. Matching two conditions
@@ -48,7 +46,6 @@ def get_sentiment(start, end, entity):
 #Loop to define start and end times. 2000ms buffer. Add 
 sent_ent = {}
 for ent in entities:
-    sent_list = ''
     start = 0
     end = int(ent['end'] + 2000)
     if int(ent['start'] - 2000 >= 0):
@@ -59,32 +56,32 @@ for ent in entities:
         sent_ent[sent[0]].append(sent[1])
     else:
         sent_ent[sent[0]] = [sent[1]]
+
 # Flatten result and average each value for each key
-real_sent_ent = {}
+avg_sent_ent = {}
 for i in sent_ent:
     flat = flatten(sent_ent[i])
     s = sum(flat)
     l = len(flat)
     if l > 0:
         avg = s / l
-        real_sent_ent[i] = avg
+        avg_sent_ent[i] = avg
 # Add the sentiment value to the dictionary based on if the average sentiment value is >0, <0, =0
 sent_and_label = {}
 sentences = []
-for i in real_sent_ent:
+for i in avg_sent_ent:
     sentiment = "neutral"
-
-    if real_sent_ent[i] < 0:
+    if avg_sent_ent[i] < 0:
         sentiment = "negative"
-        if real_sent_ent[i] < -0.5:
+        if avg_sent_ent[i] < -0.5:
             sentiment = "very negative"
-    elif real_sent_ent[i] > 0:
+    elif avg_sent_ent[i] > 0:
         sentiment = "positive"
-        if real_sent_ent[i] < 0.5:
+        if avg_sent_ent[i] < 0.5:
             sentiment = "very positive"
     
     sent_and_label[i] = {
-        "sentiment_value": real_sent_ent[i],
+        "sentiment_value": avg_sent_ent[i],
         "sentiment": sentiment
     }
     sentences.append("%s has a %s sentiment" % (i, sentiment))
@@ -137,41 +134,3 @@ col1.plotly_chart(fig)
 with col2:
     for i in sentences:
         st.markdown("- " + i)
-
-
-
-### Print Sentiment Summary (%) and respective sentiment texts ###
-
-# print('\n===| Sentiment Summary |===\n')
-# print(f'Positive Sentiment Total: {round((positive_sum / total_sum) * 100, 2)}%')
-# print(f'Neutral Sentiment Total: {round((neutral_sum / total_sum) * 100, 2)}%')
-# print(f'Negative Sentiment Total: {round((negative_sum / total_sum) * 100, 2)}%')
-
-
-# postive_sentiment =(sen_df[sen_df['sentiment']== 'POSITIVE'])
-# neutral_sentiment =(sen_df[sen_df['sentiment']== 'NEUTRAL'])
-# negative_sentiment =(sen_df[sen_df['sentiment']== 'NEGATIVE'])
-
-# print('\n===| Positive Sentiment Text + Confidence |===\n')
-
-# # print(postive_sentiment['text'])
-# print(postive_sentiment[['text', 'confidence']])
-
-# print('\n===| Negative Sentiment Text + Confidence |===\n')
-
-# # print(negative_sentiment['text'])
-# print(negative_sentiment[['text', 'confidence']])
-
-## Display sentiment score
-
-# pos_sentiment = grouped[grouped['sentiment']=='POSITIVE']['count']
-# neg_sentiment = grouped[grouped['sentiment']=='NEGATIVE']['count']
-# neu_sentiment = grouped[grouped['sentiment']=='NEUTRAL']['count']
-
-# pos_perc = round(pos_sentiment.iloc[0]*100/data_total_count)
-# neg_perc = round(neg_sentiment.iloc[0]*100/data_total_count)
-# neu_perc = round(neu_sentiment.iloc[0]*100/data_total_count)
-
-# sentiment_score = pos_perc + neu_perc - neg_perc
-# print (pos_perc , neg_perc, neu_perc)
-
